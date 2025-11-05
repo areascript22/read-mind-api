73c33949b93bad16db92c6941b27b7aaf9272b56
areascript <areascript22@gmail.com>
2025-11-04 22:25:48 -0500

    modified the service account env variable to be deployable on heroku

--- src/helpers/translate_helper.js
+++ src/helpers/translate_helper.js
@@ -1,11 +1,19 @@
-// src/services/translateService.js
 import { v2 as Translate } from "@google-cloud/translate";
 
-// Inicializa el cliente de traducción
-const translator = new Translate.Translate();
+const credentials = process.env.GOOGLE_APPLICATION_CREDENTIALS
+  ? JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
+  : null;
+
+const translator = credentials
+  ? new Translate.Translate({
+      credentials,
+      projectId: credentials.project_id,
+    })
+  : new Translate.Translate(); 
 
 export async function translateTextService(text, target = "es") {
   console.log("Calling translate service");
+
   if (!text) throw new Error("No text provided for translation");
 
   const [translation] = await translator.translate(text, target);
